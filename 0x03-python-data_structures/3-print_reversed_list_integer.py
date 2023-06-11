e7850e89b20521c82c75ed53e720d64bb79d34ce
chukwucyprian <chukwucyprian262@gmail.com>
2023-06-11 15:57:11 +0000

    correction

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,6 +3,5 @@
 
 def print_reversed_list_integer(my_list=[]):
     """Print all integers of a list in reverse order."""
-def print_reversed_list_integer(my_list=[]):
     for i in range(len(my_list) - 1, -1, -1):
         print("{0}".format(my_list[i])
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,8 +4,6 @@
 
 def no_c(my_string):
     """Remove all characters c and C from a string."""
-    new_string = ""
-    for char in my_string:
-        if char != 'c' and char != 'C':
-            new_string += char
-    return new_string
+    copy = [y for y in my_string if y != 'c' and y != 'C']
+    return ("".join(copy))
+
