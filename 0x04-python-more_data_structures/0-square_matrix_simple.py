b21e34a961885561717eda67bc924f161f159901
chukwucyprian <chukwucyprian262@gmail.com>
2023-06-15 18:38:43 +0000

    task_8_to_12

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,15 @@
-
-
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+
+    """
+    Return a new matrix with each element squared.
+
+    Args:
+        matrix (list[list]): The input matrix.
+
+    Returns:
+        list[list]: The new matrix with squared elements.
+    """
     new_matrix = matrix.copy()
 
     for i in range(len(matrix)):
