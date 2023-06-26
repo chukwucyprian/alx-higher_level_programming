9279025d7009c25a70d952a0810fa370480c25f5
chukwucyprian <chukwucyprian262@gmail.com>
2023-06-26 12:46:00 +0000

    corrected tasks

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
     try:
         count = 0
@@ -10,6 +9,6 @@ def safe_print_list(my_list=[], x=0):
                 break
     except:
         pass
-    
+
     print()
     return count
