c20cb8df9910a1495382510eb479246605356544
chukwucyprian <chukwucyprian262@gmail.com>
2023-06-27 04:18:02 +0000

    task_5 & 6

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def raise_exception():
+    """
+    Raises a TypeError exception.
+
+    Raises:
+        TypeError: Always raises a TypeError.
+
+    """
+    raise TypeError("This is a TypeError")
