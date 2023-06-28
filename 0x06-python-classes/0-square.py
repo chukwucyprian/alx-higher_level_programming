c6ef5d51b6c7fb93ad71b34dd73cf43690dc3100
chukwucyprian <chukwucyprian262@gmail.com>
2023-06-28 04:01:50 +0000

    task0_to_3

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square:
+    """
+    A class representing a square.
+
+    Attributes:
+        None
+
+    Methods:
+        None
+
+    """
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+class Square:
+    """
+    A class representing a square.
+
+    Attributes:
+        __size: Private instance attribute for the size of the square.
+
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a square with the given size.
+
+        Args:
+            size: The size of the square.
+
+        """
+        self.__size = size
