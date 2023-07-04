5cc28d8dcd73d1a60a4bf8d9a937417699c01c9c
chukwucyprian <chukwucyprian262@gmail.com>
2023-07-03 17:10:46 -0700

    task_0_to_2

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" A class that defines a rectangle."""
+"""
+A class that defines a rectangle.
+"""
 
 
 class Rectangle:
@@ -8,32 +10,3 @@ class Rectangle:
         length (float): The length of the rectangle.
         width (float): The width of the rectangle.
     """
-
-    def __init__(self, length, width):
-        """
-            Initializes a Rectangle object.
-
-            Args:
-                length (float): The length of the rectangle.
-                width (float): The width of the rectangle.
-        """
-        self.length = length
-        self.width = width
-
-    def area(self):
-        """
-            Calculates the area of the rectangle.
-
-            Returns:
-                float: The area of the rectangle.
-        """
-        return self.length * self.width
-
-    def perimeter(self):
-        """
-            Calculates the perimeter of the rectangle.
-
-            Returns:
-                float: The perimeter of the rectangle.
-        """
-        return 2 * (self.length + self.width)
