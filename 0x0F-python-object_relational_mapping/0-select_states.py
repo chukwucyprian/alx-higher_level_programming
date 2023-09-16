027bd578fbb3795c42d0372420ba11a05cf8092a
chukwucyprian <chukwucyprian262@gmail.com>
2023-09-16 20:29:24 +0100

    advanced tasks

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,9 +8,20 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT id, name FROM states")
     rows = cur.fetchall()
-    for row in rows:
-        print(row)
+
+    distinct_names = set()
+    
+    if len(rows) == 0:
+        print("No state names found.")
+    else:
+        print("State names:")
+        for row in rows:
+            state_id, state_name = row[0], row[1]
+            if state_name not in distinct_names:
+                print(f"({state_id}, '{state_name}')")
+                distinct_names.add(state_name)
+    
     cur.close()
     db.close()
