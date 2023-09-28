94ef76a799a208e78d8598531a3bbc787be7361f
chukwucyprian <chukwucyprian262@gmail.com>
2023-09-28 18:10:56 +0100

    correction

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,20 +2,23 @@
 """Finds a peak in a list of unsorted integers"""
 
 
-def find_peak(list_of_integers):
-    if not list_of_integers:
+def find_peak(list_of_int):
+    if not list_of_int:
         return None
 
-    left, right = 0, len(list_of_integers) - 1
+    left, right = 0, len(list_of_int) - 1
 
     while left < right:
         mid = (left + right) // 2
 
-        # Check if the midpoint is greater than its neighbors
-        if list_of_integers[mid] > list_of_integers[mid + 1]:
-            right = mid
-        else:
+        # Compare the midpoint with its neighbors
+        if list_of_int[mid] > list_of_int[mid + 1] and\
+                list_of_int[mid] > list_of_int[mid - 1]:
+            return list_of_int[mid]
+        elif list_of_int[mid] < list_of_int[mid + 1]:
             left = mid + 1
+        else:
+            right = mid - 1
 
     # The peak is found when left and right converge
-    return list_of_integers[left]
+    return list_of_int[left]
