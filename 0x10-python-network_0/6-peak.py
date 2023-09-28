79dd0fdad92ad4a3f5e4c2a0a01318155abfcc36
chukwucyprian <chukwucyprian262@gmail.com>
2023-09-28 17:41:28 +0100

    more tasks

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Finds a peak in a list of unsorted integers"""
+
+
+def find_peak(list_of_integers):
+    if not list_of_integers:
+        return None
+
+    left, right = 0, len(list_of_integers) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+
+        # Check if the midpoint is greater than its neighbors
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            right = mid
+        else:
+            left = mid + 1
+
+    # The peak is found when left and right converge
+    return list_of_integers[left]
